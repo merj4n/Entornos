2c618182a90ef1bd044e1e0111a43d6502526b3c
merj4n <merjan@gmail.com>
2018-05-03 20:53:11 +0200

    Subido German

--- test/EmployeeTest3.java
+++ test/EmployeeTest3.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -6,10 +7,15 @@ public class EmployeeTest3 {
 
     @Test
     public void getName() {
+        Employee Juan = new Employee(2,"Juan",22000);
 
+        Assert.assertEquals("Juan",Juan.getName());
     }
 
     @Test
     public void getSalary() {
+        Employee Manuel = new Employee(3,"Manuel",25000);
+
+        Assert.assertEquals(25000,Manuel.getSalary());
     }
 }
--- test/EmployeeTest4.java
+++ test/EmployeeTest4.java
@@ -1,3 +1,5 @@
+import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -5,6 +7,10 @@ import static org.junit.Assert.*;
 public class EmployeeTest4 {
 
     @Test
+    @Ignore
     public void getName() {
+        Employee Juan = new Employee(2,"Juan",22000);
+
+        Assert.assertEquals("Juan",Juan.getName());
     }
 }
